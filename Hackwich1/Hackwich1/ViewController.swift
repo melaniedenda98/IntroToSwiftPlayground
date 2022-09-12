376f5627919270d4fc24a373801f48ee1150460d
melaniedenda98 <mdenda@hawaii.edu>
2022-09-12 06:13:44 -0700

    Added Label to VC

---
+++ Hackwich1/Hackwich1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hackwich1
+//
+//  Created by user226981 on 9/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

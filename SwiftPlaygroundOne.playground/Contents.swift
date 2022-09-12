8d6d5d7ab36a609dcfa4a49d1e30d439c3b1cc14
melaniedenda98 <mdenda@hawaii.edu>
2022-09-12 08:18:29 -0700

    Finished Swift Playground One Assignment

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,28 @@
+// Playground a place where people can play
+
 import UIKit
 
-var greeting = "Hello, playground"
+
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+
+// We are going to do something with strings
+
+var mySecondVariable = "and look I added 2 strings together"
+var myThirdVariable = myFirstVariable + mySecondVariable
+print (myThirdVariable)
+
+// Part 8 Assign new varribles to a string
+
+var myFirstPhrase: String
+myFirstPhrase = "Hello "
+
+// The second part of the phrase
+var mySecondPhrase = "My "
+
+// The third part of the phtase
+var myThirdPhrase = "Name "
+
+// Combining the phrases into one string and printing it on a text
+var myFourthPhrase = myFirstPhrase + mySecondPhrase + myThirdPhrase
+print (myFourthPhrase)
